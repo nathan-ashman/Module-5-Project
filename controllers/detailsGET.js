ca31ddc283436e7be6075c53dbe152526ab33dd9
nathan-ashman <natetheman19@gmail.com>
2021-10-26 19:22:24 -0400

    finalized edits

--- controllers/detailsGET.js
+++ controllers/detailsGET.js
@@ -11,6 +11,8 @@ module.exports =  async (req, res)=>{
     let currCourse = await Course.findById(id);
     context.imgURL = currCourse.imgURL;
     context.username = jwt.verify(cookieObj.username, secret).username;
+    context.description = currCourse.description;
+    context.title = currCourse.title;
     let enrolledList = currCourse.usersEnrolled;
     let userId = cookieObj.userid;
     let matchingUser;
